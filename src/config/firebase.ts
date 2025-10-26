ea871ac91dc589e3d0600563c934357d29be3ad9
AlaaMatouk <contact.alaamatouk@gmail.com>
2025-10-26 19:20:58 +0300

    .

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -12,7 +13,7 @@ const firebaseConfig = {
   projectId: "car-station-6393f",
   storageBucket: "car-station-6393f.appspot.com",
   messagingSenderId: "688616261764",
-  appId: "1:688616261764:web:4d33f6b0ce57e673871ad1"
+  appId: "1:688616261764:web:c41791cca8fcd4d7871ad1",
 };
 
 // Initialize Firebase
@@ -28,4 +29,3 @@ export const db = getFirestore(app);
 export const storage = getStorage(app);
 
 export default app;
-
